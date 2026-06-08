ecfb278603be5d3183c2116a4a21fd8f97beabc6
Tahaa-Dev <tahamahmoud7097@gmail.com>
2026-06-08 05:16:26 +0300

    Feat: Add test for utils.Cache.Add() with eviction

--- utils/types_test.go
+++ utils/types_test.go
@@ -57,15 +57,12 @@ func TestCacheGet(t *testing.T) {
 	if entry.ContentType != "text/html" {
 		t.Error("Expected entry.ContentType to be 'text/html', found:", entry.ContentType)
 	}
-
 	if !bytes.Equal(entry.Data, data) {
 		t.Errorf("Unexpected entry.Data:\n%s", entry.Data)
 	}
-
 	if entry.Freq != 1 {
 		t.Errorf("Expected entry.Freq to be 1, found: %d", entry.Freq)
 	}
-
 	if cache.MinFreq != 1 {
 		t.Errorf("Unexpected cache.MinFreq: %d", cache.MinFreq)
 	}
@@ -94,5 +91,36 @@ func TestCacheAddNoEvict(t *testing.T) {
 	}
 	if cache.MinFreq != 1 {
 		t.Errorf("Unexpected cache.MinFreq2: %d", cache.MinFreq)
+	}
+}
+
+func TestCacheAddEvict(t *testing.T) {
+	cache := utils.NewCache(1)
+	filename1 := "/page.html"
+	data1 := []byte("<!DOCTYPE html>\n<html>\n<body>\n<h1>test</h1>\n</body>\n</html>")
+	cache.Add(&filename1, data1, cache.Get(&filename1))
+
+	if cache.MinFreq != 0 {
+		t.Errorf("Unexpected cache.MinFreq1: %d", cache.MinFreq)
+	}
+	if idx := bytes.Index(cache.LFUBuckets[0], []byte(filename1)); idx != 0 {
+		t.Errorf("Unexpected entry LFUBuckets[0] position: %d", idx)
+	}
+	if data := cache.Get(&filename1).Data; !bytes.Equal(data1, data) {
+		t.Errorf("Unexpected entry.Data:\n%s", data)
+	}
+
+	filename2 := "/page2.html"
+	data2 := []byte("<!DOCTYPE html>\n<html>\n<body>\n<h2>test2</h2>\n</body>\n</html>")
+	cache.Add(&filename2, data2, cache.Get(&filename2))
+
+	if cache.MinFreq != 1 {
+		t.Errorf("Unexpected cache.MinFreq: %d", cache.MinFreq)
+	}
+	if idx := bytes.Index(cache.LFUBuckets[1], []byte(filename2)); idx != 0 {
+		t.Errorf("Unexpected entry LFUBuckets[1] position: %d", idx)
+	}
+	if data := cache.Get(&filename2).Data; !bytes.Equal(data2, data) {
+		t.Errorf("Unexpected entry.Data:\n%s", data)
 	}
 }
